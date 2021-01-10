7067455c5694b08c9d7aeb9f5fc08d1da9318284
NadunN <n.nadun@theglobalmarketaccess.com>
2021-01-11 00:32:50 +0530

    11-01-2021 | routing and blog posts

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const _ = require("lodash");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -15,13 +16,70 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+//global variables
+const posts = [];
 
 
 
+//home GET route
+app.get("/", function(req, res){
 
+  res.render("home", {
+    homeContent : homeStartingContent,
+    posts : posts
+  });
+});
 
+//about GET route
+app.get("/about", function(req, res){
+  res.render("about", {
+    aboutContent : homeStartingContent
+  });
+});
 
+//contact GET route
+app.get("/contact", function(req, res){
+  res.render("contact", {
+    contactContent : homeStartingContent
+  });
+});
 
+//compose GET route
+app.get("/compose", function(req, res){
+  res.render("compose");
+});
+
+app.get("/posts/:postName", function(req, res){
+  const requestedTitle = _.lowerCase(req.params.postName);
+
+  posts.forEach(function(post){
+    const storedTitle = _.lowerCase(post.title);
+
+    const sendTitle = post.title;
+    const sendBody = post.body;
+
+    if(storedTitle === requestedTitle){
+      res.render("post", {
+        sendTitle : sendTitle,
+        sendBody : sendBody
+      });
+    }
+  });
+  
+});
+
+//compose POST route
+app.post("/compose", function(req, res){
+  let composeTitle = req.body.postTitle;
+  let composePost = req.body.postBody;
+  //adding input data in to a newly created object
+  const post = {
+    title: composeTitle,
+    body: composePost
+  }
+  posts.push(post);
+  res.redirect("/");
+});
 
 
 
